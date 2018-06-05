8dd6072757dd3efbc7ca5f5ce62d8da7a910d92b
Andrii <kinjin@iMac-Andrii.local>
2018-06-05 14:08:35 +0300

    mac.sh

---
+++ mac.sh
@@ -0,0 +1,20 @@
+#install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#install homebrew cask
+brew tap caskroom/cask
+
+#cask apps
+brew cask install google-chrome
+brew cask install firefox
+brew cask install telegram
+brew cask install tunnelbear
+brew cask install visual-studio-code
+brew cask install iterm2
+brew cask install slack
+brew cask install spotify
+brew cask install docker
+brew cask install atom
+brew cask install postman
+brew cask install skype
+brew cask install sublime-text
+brew cask install hyper
